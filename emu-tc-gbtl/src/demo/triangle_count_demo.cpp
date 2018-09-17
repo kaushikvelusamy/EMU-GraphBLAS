ad629193848c5555101f60a82f9ab30b6e6b2c52
kaushik <kaushik@linux.com>
2018-09-17 18:15:48 -0400

    Added Makefile, profiler information, fstream to read input

--- emu-tc-gbtl/src/demo/triangle_count_demo.cpp
+++ emu-tc-gbtl/src/demo/triangle_count_demo.cpp
@@ -2,22 +2,24 @@
 #include <string>
 #include <vector>
 #include <stdio.h>
-
+#include<fstream>
 
 #include <graphblas/graphblas.hpp>
 #include <algorithms/triangle_count.hpp>
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
+int main(int argc, char **argv) {
+    starttiming();
+
+    if (argc < 2) {
         std::cerr << "ERROR: too few arguments." << std::endl;
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
         exit(1);
     }
 
+    std::string pathname(argv[1]);
+    std::ifstream infile(pathname);
     // Read the edgelist and create the tuple arrays
     GraphBLAS::IndexArrayType iL, iU, iA;
     GraphBLAS::IndexArrayType jL, jU, jA;
@@ -27,31 +29,24 @@ int main(int argc, char **argv)
 
     GraphBLAS::IndexArrayType :: iterator i;
 
-
-
-    FILE *infile = fopen(argv[1], "r");
-    if (!infile)
-    {
+    if (!infile) {
         fprintf(stderr, "Unable to open file: %s\n", argv[1]);
         exit(1);
     }
 
-    while ( fscanf(infile, "%ld %ld", &src, &dst) == 2)
-    {
-        printf("Read: %ld, %ld\n", src, dst);
+    while (infile) {
+        infile >> src >> dst;
+        std::cout << "Read: " << src << ", " << dst << std::endl;
         if (src > max_id) max_id = src;
         if (dst > max_id) max_id = dst;
 
-        if (src < dst)
-        {
+        if (src < dst) {
             iA.push_back(src);
             jA.push_back(dst);
 
             iU.push_back(src);
             jU.push_back(dst);
-        }
-        else if (dst < src)
-        {
+        } else if (dst < src) {
             iA.push_back(src);
             jA.push_back(dst);
 
@@ -62,44 +57,44 @@ int main(int argc, char **argv)
 
         ++num_rows;
     }
-    fclose(infile);
+
     std::cout << "Read " << num_rows << " rows." << std::endl;
     std::cout << "#Nodes = " << (max_id + 1) << std::endl;
 
     std::cout <<"iA Vector";
     for (i = iA.begin(); i != iA.end(); ++i)
         std::cout <<*i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     std::cout <<"jA Vector";
     for (i = jA.begin(); i != jA.end(); ++i)
         std::cout << *i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     std::cout <<"iU Vector";
     for (i = iU.begin(); i != iU.end(); ++i)
         std::cout <<*i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     std::cout <<"jU Vector";
     for (i = jU.begin(); i != jU.end(); ++i)
         std::cout <<*i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     std::cout <<"iL Vector";
     for (i = iL.begin(); i != iL.end(); ++i)
         std::cout <<*i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     std::cout <<"jL Vector";
     for (i = jL.begin(); i != jL.end(); ++i)
         std::cout <<*i << '\t';
-    std::cout<<std::endl;
+    std::cout<< std::endl;
 
 
     GraphBLAS::IndexType NUM_NODES(max_id + 1);
@@ -109,9 +104,9 @@ int main(int argc, char **argv)
     /// @todo change scalar type to unsigned int or GraphBLAS::IndexType
     typedef GraphBLAS::Matrix<T, GraphBLAS::DirectedMatrixTag> MatType;
 
-    //MatType A(NUM_NODES, NUM_NODES);
+    // MatType A(NUM_NODES, NUM_NODES);
     MatType L(NUM_NODES, NUM_NODES);
-    //MatType U(NUM_NODES, NUM_NODES);
+    // MatType U(NUM_NODES, NUM_NODES);
 
     //    A.build(iA.begin(), jA.begin(), v.begin(), iA.size());
     L.build(iL.begin(), jL.begin(), v.begin(), iL.size());
